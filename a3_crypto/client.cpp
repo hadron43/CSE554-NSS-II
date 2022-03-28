1ad6d497d16a29257babb502cc738761af60e3ec
Harsh Kumar <hadron43@yahoo.com>
2022-03-28 23:02:40 +0530

    ass3: cryptography + IPC

--- a3_crypto/client.cpp
+++ a3_crypto/client.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 #include <pthread.h>
+#include <semaphore.h>
 using namespace std;
 
 #include "crypto.cpp"
+
+int fd[2];
+char *filepath = nullptr, *ip = nullptr, *port = nullptr, *static_key = nullptr;
 
 void process_packet(string packet, string psk) {
     string iv, hmac, enc_message;
@@ -24,23 +28,63 @@ void process_packet(string packet, string psk) {
     decrypt(enc_message);
 }
 
-int main(int argc, char* argv[]) {
-    string passphrase, message, enc_message, hmac;
+void read_func() {
+    // read file into buffer, encrypt it, store into enc_shared_buff
+    ifstream fin;
+    string psk = static_key;
 
-    cout << "Enter passphrase: ";
-    cin >> passphrase;
-    cout << "Enter message: ";
-    cin >> message;
-    initialize(passphrase, true);
+    fin.open(filepath);
+    if(!fin) {
+        perror("open");
+        return;
+    }
 
-    enc_message = encrypt(message);
-    hmac = calculate_hmac(enc_message);
+    int ind = 0;
+    char c;
+    string buff;
+    while(!fin.eof()) {
+        fin.get(c);
+        if(fin.eof())
+            break;
+        buff += c;
+    }
 
+    fin.close();
+
+    initialize(psk);
+    string enc_message = encrypt(buff);
+    string hmac = calculate_hmac(enc_message);
     string packet = get_iv() + hmac + enc_message;
-    cout << "packet: " << packet << endl;
-    ofstream fout("enc");
-    fout.write(packet.c_str(), packet.length());
-    fout.close();
-
     destruct_final();
+
+    write(fd[1], packet.c_str(), packet.length());
+}
+
+void send_func() {
+    // send message from enc_shared_buff
+    dup2(fd[0], STDIN_FILENO);
+    execl("/bin/nc", "", ip, port, NULL);
+    perror("execl");
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 5) {
+        cerr << "usage: ./client <key> <filepath> <ip> <port>\n";
+        exit(-1);
+    }
+    static_key = argv[1];
+    filepath = argv[2];
+    ip = argv[3];
+    port = argv[4];
+
+    pipe(fd);
+    if(fork() == 0) {
+        read_func();
+    }
+    else {
+        send_func();
+    }
+
+    close(fd[0]);
+    close(fd[1]);
 }
--- a3_crypto/server.cpp
+++ a3_crypto/server.cpp
@@ -1,49 +1,96 @@
 #include <bits/stdc++.h>
 #include <pthread.h>
+#include <semaphore.h>
 using namespace std;
 
 #include "crypto.cpp"
 
-int main(int argc, char* argv[]) {
-    string passphrase, enc_message, hmac, iv;
-    char c;
+int fd[2];
+char *filepath = nullptr, *port = nullptr, *static_key = nullptr;
 
-    cout << "Enter passphrase: ";
-    cin >> passphrase;
-
-    ifstream fin("enc", ios::in | ios::binary);
-
-    char buff[1024];
-    memset(buff, 0, sizeof(buff));
-    int ind = 0;
-    while(!fin.eof()) {
-        fin.get(c);
-        if(fin.eof())
-            break;
-        buff[ind ++] = c;
-    }
-
-    fin.close();
-
+void process_packet(string packet, string psk) {
+    string iv, hmac, enc_message;
     int i = 0;
     while(i < IV_SIZE)
-        iv += buff[i++];
+        iv += packet[i++];
     while(i < IV_SIZE + HMAC_SIZE)
-        hmac += buff[i++];
-    while(i < ind)
-        enc_message += buff[i++];
+        hmac += packet[i++];
+    while(i < packet.length())
+        enc_message += packet[i++];
 
-    cout << "buff: " << buff << "$" << endl
-        << "key: " << passphrase << endl
-        << "iv: " << iv << endl
-        << "hmac: " << hmac << endl
-        << "mes: " << enc_message << endl;
     // cout << "iv: " << iv << "$" << endl;
 
-    initialize(passphrase, iv, true);
+    initialize(psk, iv, true);
+
+    // cout << "iv: " << ::iv << "$" << endl;
 
     verify_hmac(enc_message, hmac);
     decrypt(enc_message);
+}
 
-    destruct_final();
+void translate_func() {
+    // read from buffer, decrypt it, store into file
+    ofstream fout;
+    string psk = static_key;
+
+    fout.open(filepath);
+    if(!fout) {
+        perror("open");
+        return;
+    }
+
+    int ind = 0;
+    char c;
+    char packet[MAX_MESSAGE_SIZE];
+    int count = read(fd[0], packet, MAX_MESSAGE_SIZE);
+
+    close(fd[0]);
+
+    string iv, hmac, enc_message;
+    int i = 0;
+    while(i < IV_SIZE)
+        iv += packet[i++];
+    while(i < IV_SIZE + HMAC_SIZE)
+        hmac += packet[i++];
+    while(i < count)
+        enc_message += packet[i++];
+
+    initialize(psk, iv, true);
+    if(!verify_hmac(enc_message, hmac)) {
+        cerr << "HMAC Failed!" << endl;
+        fout.close();
+        return;
+    }
+    string message = decrypt(enc_message);
+
+    fout.write(message.c_str(), message.length());
+    fout.close();
+}
+
+void recv_func() {
+    // send message from enc_shared_buff
+    dup2(fd[1], STDOUT_FILENO);
+    execl("/bin/nc", "", "-l", port, NULL);
+    perror("execl");
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 4) {
+        cerr << "usage: ./server <key> <filepath> <port>\n";
+        exit(-1);
+    }
+    static_key = argv[1];
+    filepath = argv[2];
+    port = argv[3];
+
+    pipe(fd);
+    if(fork() == 0) {
+        translate_func();
+    }
+    else {
+        recv_func();
+    }
+
+    close(fd[0]);
+    close(fd[1]);
 }
